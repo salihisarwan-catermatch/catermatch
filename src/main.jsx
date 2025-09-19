b02ff6d4692ef7c6103a7a6172ba724bec07b036
sarwan salihi <salihisarwan@gmail.com>
2025-09-19 17:37:36 +0200

    Enable Vercel Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics /> 
     </BrowserRouter>
   </React.StrictMode>
 );
